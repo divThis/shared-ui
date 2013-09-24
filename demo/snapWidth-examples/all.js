b7482880601fbfa70d874bb1f666258c1f83e9e9
Christopher M. Mitchell <christopher@divthis.com>
2013-09-24 16:00:29 -0700

    Removing adaptive/non-adaptive examples due to removal of fixed grid in Bootstrap 3.

--- demo/snapWidth-examples/all.js
+++
@@ -1,6 +0,0 @@
-define(['./overview', './adaptive-2width', './non-adaptive'], function() {
-    return {
-        title: 'Snap Width',
-        examples: Array.prototype.slice.call(arguments)
-    };
-});
